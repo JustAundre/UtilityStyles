318ace0902508eec72252356bbd4b328e5ba12b3
JustAundre <126304209+JustAundre@users.noreply.github.com>
2024-11-02 18:19:13 -0500

    Website Redesign

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 // Defines elements
-let packDetails = document.getElementById("packDetailsId");
-let permissions = document.getElementById("termsOfUseId");
-let changelog = document.getElementById("changelogId");
+const packDetails = document.querySelector(".packDetails");
+const permissions = document.querySelector(".termsOfUse");
+const updateLog = document.querySelector(".updateLog");
+
 function detailsClick() {
   // Sets the button to inactive if it was already active and was clicked.
   if (packDetails.hasAttribute("active")) {
@@ -11,7 +12,7 @@ function detailsClick() {
   else {
   packDetails.setAttribute("active", "")
   permissions.removeAttribute("active")
-  changelog.removeAttribute("active")
+  updateLog.removeAttribute("active")
   }
 }
 function permissionsClick() {
@@ -34,3 +35,31 @@ function changelogClick() {
   changelog.setAttribute("active", "")
   }
 }
+
+function generateRandomValues() {
+  const x1 = Math.round(Math.random() * 100) - 50;
+  const x2 = Math.round(Math.random() * 100) - 50;
+  const x3 = Math.round(Math.random() * 100) - 50;
+  const y1 = Math.round(Math.random() * 100) - 50;
+  const y2 = Math.round(Math.random() * 100) - 50;
+  const y3 = Math.round(Math.random() * 100) - 50;
+
+  document.documentElement.style.setProperty(
+    '--x1', `${x1}vw` );
+  document.documentElement.style.setProperty(
+    '--x2', `${x2}vw` );
+  document.documentElement.style.setProperty(
+    '--x3', `${x3}vw` );
+  document.documentElement.style.setProperty(
+    '--y1', `${y1}vh` );
+  document.documentElement.style.setProperty(
+    '--y2', `${y2}vh` );
+  document.documentElement.style.setProperty(
+    '--y3', `${y3}vh` );
+}
+
+// Call the function initially to set the initial values
+generateRandomValues();
+
+// Set an interval to update the values every 20 seconds
+setInterval(generateRandomValues, 20000);
